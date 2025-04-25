12d55228a55ed2a70bd13356c61bd5f6d3a2468d
mauri-1973 <mauri-1973@outlook.cl>
2025-04-25 01:12:54 -0400

    actualizado correctamente

--- app/Http/Controllers/Docs/DocumentsExtController.php
+++ app/Http/Controllers/Docs/DocumentsExtController.php
@@ -45,6 +45,8 @@ use Illuminate\Support\Facades\Crypt;
 
 use Session;
 
+use Illuminate\Support\Facades\Artisan;
+
 
 class DocumentsExtController extends Controller
 
@@ -132,6 +134,12 @@ class DocumentsExtController extends Controller
             array_push($array, array('titulo' => trans('inst.'.$dat->titulo), 'imagen' => $dat->img, 'inst' => trans('inst.'.$dat->inst), 'url' => trans('inst.'.$dat->nombreurl) ) );
         }
         return response()->json(['ok' => 'Datos Enviados', 'datos' => $datos, 'array' => $array], 200);
+    }
+
+    public function storgelink()
+    {
+        Artisan::call('storage:link');
+        dd('link simbólico realizado con éxito 2');
     }
 
 }
